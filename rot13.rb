38b78c084477d0fd76cf6ba274b8dffb94e45f21
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-09-24 08:57:51 -0400

    clean up email a little may not function

--- rot13.rb
+++ rot13.rb
@@ -1,6 +1,8 @@
+require 'optparse' 
 def is_letter?(chr)
     return (chr =~ /[A-Za-z]/) != nil
 end
+
 def encode(chr)
     downs = 'abcdefghijklmnopqrstuvwxyz'
     ups = downs.upcase
@@ -12,21 +14,19 @@ def encode(chr)
         return ups[new_index]
     end
 end
+
+def encode_line(line)
+    return line.each do |chr|
+        is_letter? chr ? encode chr : chr
+    end
+end
+
 p ARGV[0]
 out_name = File.basename(ARGV[0], File.extname(ARGV[0])) + '_decrypt' + File.extname(ARGV[0])
 out = File.open(out_name, 'a')
 File.open(ARGV[0], 'r') do |file|
     file.each_line do |line|
-        chars = line.split('')
-        oline = ''
-        chars.each do |char|
-            if is_letter?(char)
-                oline += encode char
-            else
-                oline += char
-            end
-        end
-        out.write(oline)
+        out.write encode_line line
     end
 end
 out.close()
